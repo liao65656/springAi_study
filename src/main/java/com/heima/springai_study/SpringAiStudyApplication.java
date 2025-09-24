182013a2986edb39bb81122d313e3c73439aee5e
weijie.liao <2269184071@qq.com>
2025-09-24 19:44:22 +0800

    spring ai firstCommit

---
+++ src/main/java/com/heima/springai_study/SpringAiStudyApplication.java
@@ -0,0 +1,13 @@
+package com.heima.springai_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiStudyApplication.class, args);
+    }
+
+}
