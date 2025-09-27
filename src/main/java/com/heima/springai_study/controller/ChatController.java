494362f838c4ce5bc0a4667e12961122f0673aa3
weijie.liao <2269184071@qq.com>
2025-09-27 10:31:10 +0800

    实现向量化存储以及搜索，支持用户根据上下文以及知识库搜索回答

--- src/main/java/com/heima/springai_study/controller/ChatController.java
+++ src/main/java/com/heima/springai_study/controller/ChatController.java
@@ -10,6 +10,9 @@ import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.deepseek.DeepSeekChatModel;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,6 +29,12 @@ public class ChatController {
     private final DeepSeekChatModel chatModel;
     @Autowired
     private IHistoryService historyService;
+    @Resource
+    private EmbeddingModel embeddingModel;
+    @Resource
+    private VectorStore vectorStore;
+    @Resource
+    private ChatClient chatClient;
 
     @Autowired
     public ChatController(DeepSeekChatModel chatModel) {
@@ -42,12 +51,20 @@ public class ChatController {
     @GetMapping(value = "/ai/generateStream",produces = "text/html;charset=UTF-8")
 	public Flux<String> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message,
                                        @RequestParam(value="sessionId",defaultValue = "1")Long sessionId)      {
+
+        List<Document> documents = vectorStore.similaritySearch(message);
+        StringBuilder contextBuilder=new StringBuilder();
+        documents.forEach(document -> {
+            contextBuilder.append(document).append("\n");
+        });
         History userHistory=new History();
         userHistory.setContent(message);
         userHistory.setRole("user");
         userHistory.setDatetime(LocalDateTime.now());
         userHistory.setSessionId(sessionId);
-        String promote="你是白胡子船队下的第三船队队长砖石乔兹，你作为ai助手要对用户发起的请求配合剧情回答"+"\n请根据上面内容展开回答";
+        String promote="你是编程助手，世界上最强海贼白胡子!，以下是本次会话传递的信息:"+contextBuilder.toString()+
+                "\n请根据以上内容进行展开，回答下面的问题";
+
         historyService.save(userHistory);
         List<History> historyList = historyService.list(new LambdaQueryWrapper<History>().eq(History::getSessionId,sessionId));
         List<Message> userMessageList = historyList.stream().map(history->
